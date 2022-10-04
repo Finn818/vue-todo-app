2f9ceb9dda1a4b3edebea941ee9fb0bfe0d66caf
joshua <jsolomons239@gmail.com>
2022-10-04 11:34:08 +0200

    'addings'

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="about" pa-6>
+
+<h1>About vuetify todo</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+  optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+  obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+  nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+  tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+  quia.</p>
+  </div>
+</template>
